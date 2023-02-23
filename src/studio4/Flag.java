9b30f7a3ede90088d3be704938f66d9c3c9bde99
yulonglin0412 <l.yulong@wustl.edu>
2023-02-23 12:38:42 -0600

    studio4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,12 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.rectangle(.5, .5, .3, .2);
+		StdDraw.setPenColor(Color.ORANGE);
+		StdDraw.filledCircle(.3, .6, .05);
+		StdDraw.line(.3,.47,.3,.53);
+		StdDraw.line(.37,.6,.43,.6);
+		StdDraw.line(.36,.55,.4,.50);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +21,44 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		
+		boolean ifFilled = in.nextBoolean();
+		double[] point = new double[6];
+		int i = 0;
+		while(in.hasNextDouble()) {
+			point[i] = in.nextDouble();
+			i++;
+		}
+		
+		Color newColor = new Color(red, green, blue);
+		StdDraw.setPenColor(newColor);
+		if (shape.equals("ellipse")) {
+			if (ifFilled) {
+				StdDraw.filledEllipse(point[0], point[1],point[2],point[3]);
+			} else {
+				StdDraw.ellipse(point[0], point[1],point[2],point[3]);
+			}
+		} else if (shape.equals("rectangle")) {
+			if (ifFilled) {
+				StdDraw.filledRectangle(point[0], point[1],point[2],point[3]);
+			} else {
+				StdDraw.rectangle(point[0], point[1],point[2],point[3]);
+			}
+		} else if (shape.equals("triangle")) {
+			double[] x = {point[0], point[2],point[4]};
+			double[] y = {point[1], point[3],point[5]};
+			if (ifFilled) {
+				StdDraw.filledPolygon(x,y);
+			} else {
+				StdDraw.polygon(x,y);
+			}
+		}
+
+
 	}
 }
+	
